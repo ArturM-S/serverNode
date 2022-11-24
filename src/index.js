93c77eef0ad18c2a556882d357d40cfd4f0dc3d3
ArturM-S <silvamarquesartur@gmail.com>
2022-11-24 08:57:55 -0300

    allowCrossDomain

--- src/index.js
+++ src/index.js
@@ -10,7 +10,24 @@ var cors = require('cors')
 const jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(cors())
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
+//...
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'cool beans' }));
+  app.use(express.methodOverride());
+  app.use(allowCrossDomain);
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
